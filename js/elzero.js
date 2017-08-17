cfb581053742fcaf81f39e2b00292991397110d3
DESKTOP-4KP52D6\Ahmed Ortega <ahmedsniper2020@outlook.com>
2017-08-17 12:22:41 +0200

    Our Location Finished
    Lolll

---
+++ js/elzero.js
@@ -0,0 +1,15 @@
+/*global $, jQuery, console, alert, prompt */
+$(document).ready(function () {
+    'use strict';
+    $(window).on("resize", function () {
+        var viewport_width = $(window).width();
+        if (viewport_width < 1200 && viewport_width > 760) {
+            console.log(viewport_width);
+            $(".ourteam .leadership ul li").attr("id", "remover");
+        } else if (viewport_width < 760) {
+            $(".ourteam .leadership ul li").attr("id", "xs");
+        } else {
+            $(".ourteam .leadership ul li").attr("id", "");
+        }
+    });
+});
