21750a056d4090e905e52ace5a938370a80b94cc
DESKTOP-4KP52D6\Ahmed Ortega <ahmedsniper2020@outlook.com>
2017-08-17 21:25:26 +0200

    Loading Page finished

--- js/elzero.js
+++ js/elzero.js
@@ -1,9 +1,13 @@
 /*global $, jQuery, console, alert, prompt */
 $(document).ready(function () {
     'use strict';
+    
+    var viewport_width = $(window).width(),
+        ColorLi = $(".option_box .color_option ul li");
+    
     $(window).on("resize", function () {
-        var viewport_width = $(window).width();
         if (viewport_width < 1200 && viewport_width > 760) {
+            
             console.log(viewport_width);
             $(".ourteam .leadership ul li").attr("id", "remover");
         } else if (viewport_width < 760) {
@@ -12,9 +16,7 @@ $(document).ready(function () {
             $(".ourteam .leadership ul li").attr("id", "");
         }
     });
-    var viewport_width = $(window).width(),
-        ColorLi = $(".option_box .color_option ul li");
-        
+
     if (viewport_width < 1200 && viewport_width > 760) {
         console.log(viewport_width);
         $(".ourteam .leadership ul li").attr("id", "remover");
@@ -23,18 +25,37 @@ $(document).ready(function () {
     } else {
         $(".ourteam .leadership ul li").attr("id", "");
     }
-    
+
     /*  Tool Box  */
     $(".option_box .icon_box").click(function () {
         $(".option_box .color_option").fadeToggle();
     });
-    
-    
+
+
     ColorLi.eq(0).css("backgroundColor", "#C62727").end().eq(1).css("backgroundColor", "#4508ff").end().eq(2).css("backgroundColor", "#ff0fc0").end();
-    
+
     ColorLi.click(function () {
         $("link[href*='theme']").attr("href", $(this).attr('data-value'));
     });
-    
-    
+});
+
+
+
+$(window).on("load", function () {
+
+    "use strict";
+
+    // Loading Elements
+
+    $(".loading-overlay .sk-cube-grid").fadeOut(2000, function () {
+
+        // Show The Scroll
+
+        $("body").css("overflow", "auto");
+
+        $(this).parent().fadeOut(2000, function () {
+
+            $(this).remove();
+        });
+    });
 });
