98eea9711802a17bd6dba0ac3070e4fb69635a22
DESKTOP-4KP52D6\Ahmed Ortega <ahmedsniper2020@outlook.com>
2017-08-17 12:37:17 +0200

    some change

--- js/elzero.js
+++ js/elzero.js
@@ -12,4 +12,13 @@ $(document).ready(function () {
             $(".ourteam .leadership ul li").attr("id", "");
         }
     });
+    var viewport_width = $(window).width();
+    if (viewport_width < 1200 && viewport_width > 760) {
+        console.log(viewport_width);
+        $(".ourteam .leadership ul li").attr("id", "remover");
+    } else if (viewport_width < 760) {
+        $(".ourteam .leadership ul li").attr("id", "xs");
+    } else {
+        $(".ourteam .leadership ul li").attr("id", "");
+    }
 });
